f62e1431eb71e9c437bcfd1b895df1af673b8f90
Saipang Chan <Saipang.Chan@nttdata.com>
2021-05-14 02:51:35 +0900

    Importing Your Own Files

--- notes-app/app.js
+++ notes-app/app.js
@@ -8,3 +8,13 @@ fs.writeFileSync('notes.txt', 'My name is Pango Chan!')
 
 fs.appendFileSync('notes.txt', ' I just added one message!!')
 fs.appendFileSync('notes.txt', ' Oh, I again just added another message!!!')
+
+const add = require('./utils.js')
+
+const sum = add(4, 2)
+console.log(sum)
+
+// Challenge: Define and use a function in a new file
+const getNotes = require('./notes.js')
+const notes = getNotes()
+console.log(notes)
---
+++ notes-app/utils.js
@@ -0,0 +1,10 @@
+console.log('utils.js')
+
+const name = 'Utils'
+//module.exports = name
+// call: const name = require('./utils.js')
+
+const add = function (a, b) {
+    return a + b
+}
+module.exports = add

efbed4706bab5697ff09b71eecb01a1715350879
Saipang Chan <Saipang.Chan@nttdata.com>
2021-05-16 01:17:56 +0900

    Argument Parsing with yargs

--- notes-app/app-console.js
+++ notes-app/app-console.js
@@ -1,11 +1,43 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 const log = console.log
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    log('Adding note!')
-} else if (command === 'remove') {
-    log('Removing note!')
-}
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        log('Adding a note')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        log('Removing the note')
+    }
+})
+
+// Challenge: Add two new commands
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function() {
+        log('Listing the notes')
+    }
+})
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        log('Reading a note')
+    }
+})
+
+console.log(yargs.argv)

449e2337db86588cbdf6b895f1ef437c6b5ffbd6
Saipang Chan <Saipang.Chan@nttdata.com>
2021-05-27 00:46:39 +0900

    Promises

---
+++ playground/8-promises.js
@@ -0,0 +1,39 @@
+
+// Callback version
+
+const doWorkCallback = (callback) => {
+    setTimeout(() => {
+        // callback('This is my error!', undefined)
+        callback(undefined, [1, 4, 7])
+    }, 2000)
+}
+
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(result)
+})
+
+
+// Promise version as improvement of callback pattern
+
+const doWorkPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([7, 4, 1])
+        reject('Things went wrong!')
+    }, 2000)
+})
+
+doWorkPromise.then((result) => {
+    console.log('Success!', result)
+}).catch((error) => {
+    console.log('Error!', error)
+})
+
+//                             fulfilled
+//                            /
+// Promise      -- pending -->
+//                            \
+//                             rejected
